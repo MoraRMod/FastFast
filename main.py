ae8ee9fd5cbb593a109f60f71c68ec793a88d324
MoraRMod <ralerwip@gmail.com>
2023-05-17 21:01:52 -0600

    Se hace prueba para ver como se imprimen los datos

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ from supplier import Supplier
 from employee import Employee
 from ticket import Ticket
 from bill import Bill
+from manager import Manager
 
-prueba = []
-hora = Hour('09', '45')
+cliente = Client('102364', 'Omar', 'DOJA98DHHF9', '56 4651 7427', 'ralerwip@gmail.com')
 fecha = Date('17', '05', '2023')
-nombre = Employee()
-producto = Product()
-cliente = Client()
+hora = Hour('09', '45')
+producto = Product('95', '17', 'Takis', '0110111011')
+proovedor = Supplier('Barcel', 'Primavera #63', 'ayudabarcel@ayuda.com', '54 8499 0008')
+empleado = Employee('948367', fecha, 'Daniel', '5 de mayo #92', 'KAJN15ENMI4', 'soyDani@gmail.com', '33 1594 8269')
+recibo = Ticket('492816', empleado.getName(), hora, fecha, producto.getCode(), producto.getName(), 'Tarjeta de credito')
+prueba = Bill(hora, fecha, empleado.getName(), producto.getName(), cliente.getName())
+gerente = Manager()
+gerente.agregar(cliente, fecha, hora, producto, proovedor, empleado, recibo, prueba)
 
-nombre.setName('Omar')
-producto.setName('Galletas')
-cliente.setName('Yair')
-
-prueba = Bill(hora, fecha, nombre.getName(), producto.getName(), cliente.getName())
-
-print(prueba)
+print('\n')
+gerente.mostrar()
+print('\n')
