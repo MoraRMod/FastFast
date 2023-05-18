c4d9314e646c878e34c5750fcaab3a99b8da5d5a
MoraRMod <ralerwip@gmail.com>
2023-05-18 13:06:01 -0600

    un modulo solo para otros modulos

---
+++ modules.py
@@ -0,0 +1,11 @@
+from client import Client
+from date import Date
+from hour import Hour
+from product import Product
+from supplier import Supplier
+from employee import Employee
+from ticket import Ticket
+from bill import Bill
+from indiceSimple import getIndex
+from indiceInvertido import actualizarIndiceInvertido, mostrarIndiceInvertido
+from manager import Manager
