0697e8ddf28f78f57b1c7901c2fa2ecd02c4591a
MoraRMod <ralerwip@gmail.com>
2023-05-18 12:44:14 -0600

    funciones para acceder a los indices invertidos

---
+++ indiceInvertido.py
@@ -0,0 +1,24 @@
+from client import Client
+
+def actualizar_indice_invertido(manager):
+	indice_invertido = {}
+	for i, registro in enumerate(manager):
+		for obj in registro:
+			if isinstance(obj, Client):
+				nombre = obj.getName()
+				if nombre not in indice_invertido:
+					indice_invertido[nombre] = []
+				indice_invertido[nombre].append(i)
+	return indice_invertido
+
+def mostrar_indice_invertido(manager, indice_invertido):
+    print("Índice invertido:")
+    for nombre, indices in indice_invertido.items():
+        print(f"Registros para el cliente '{nombre}':")
+        for indice in indices:
+            registro = manager.getIndex(indice)
+            print(f"Índice asociado: {indice}")
+            for obj in registro:
+                print(obj)
+            print()
+
