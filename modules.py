f5a21a3de0367814b2e9400c0390b3a4c2556141
MoraRMod <ralerwip@gmail.com>
2023-05-18 23:49:58 -0600

    Se seccionan los modulos y se agrega encryption y otros mas

--- modules.py
+++ modules.py
@@ -1,3 +1,4 @@
+# Clases
 from client import Client
 from date import Date
 from hour import Hour
@@ -6,8 +7,18 @@ from supplier import Supplier
 from employee import Employee
 from ticket import Ticket
 from bill import Bill
+from manager import Manager
+
+# Algoritmos
 from plainIndex import getIndex
 from invertedIndex import actualizarIndiceInvertido, mostrarIndiceInvertido
 from serialization import serializar, deserializar
 from dispersion import dispersar, mostrarDispersado
-from manager import Manager
+from encryption import generarClave, encriptar, desencriptar
+
+# Librerias de ayuda
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import json
+import pickle
