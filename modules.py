8a0acd8d7a68374c913450dd07463fa405ed5bd7
MoraRMod <ralerwip@gmail.com>
2023-05-19 00:16:30 -0600

    Se agrega el modulo de compression

--- modules.py
+++ modules.py
@@ -15,6 +15,7 @@ from invertedIndex import actualizarIndiceInvertido, mostrarIndiceInvertido
 from serialization import serializar, deserializar
 from dispersion import dispersar, mostrarDispersado
 from encryption import generarClave, encriptar, desencriptar
+from compression import comprimir, descomprimir
 
 # Librerias de ayuda
 from cryptography.fernet import Fernet
