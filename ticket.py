6cdb8dc24442d9329a41e2a18bcc2131828401b4
MoraRMod <ralerwip@gmail.com>
2023-05-20 14:42:08 -0600

    productValue ++

--- ticket.py
+++ ticket.py
@@ -1,12 +1,13 @@
 class Ticket:
 	# Agrega datos por clase
-	def __init__(self, id = "", employee = "", hour = None, date = None, productCode = "", product = "", methodPayment = "") -> None:
+	def __init__(self, id = "", employee = "", hour = None, date = None, productCode = "", productName = "", productValue = "", methodPayment = "") -> None:
 		self.__id = id
 		self.__employee = employee
 		self.__hour = hour
 		self.__date = date
 		self.__productCode = productCode
-		self.__product = product
+		self.__productName = productName
+		self.__productValue = productValue
 		self.__methodPayment = methodPayment
 	# Comparacion en caso de ordenamiento por ID
 	def __lt__(self, other):
@@ -19,7 +20,8 @@ class Ticket:
 			"Hora: " + str(self.__hour) + "\n" +
 			"Fecha: " + str(self.__date) + "\n" +
 			"Código del producto: " + str(self.__productCode) + "\n" +
-			"Producto: " + str(self.__product) + "\n" +
+			"Producto: " + str(self.__productName) + "\n" +
+			"Precio: " + str(self.__productValue) + "\n" +
 			"Método de pago: " + str(self.__methodPayment)
 		)
 	# Guardar en disco
@@ -30,13 +32,20 @@ class Ticket:
 			"hour": self.__hour.toDict() if self.__hour else None,
 			"date": self.__date.toDict() if self.__date else None,
 			"productCode": self.__productCode,
-			"product": self.__product,
-			"methodPayment": self.__methodPayment,
+			"productName": self.__productName,
+			"productValue": self.__productValue,
+			"methodPayment": self.__methodPayment
 		}
 	
 	# SETTERS
 	def setID(self, id):
 		self.__id = id
+
+	def setHour(self, hour):
+		self.__hour = hour
+
+	def setDate(self, date):
+		self.__date = date
 	
 	def setEmployee(self, employee):
 		self.__employee = employee
@@ -44,37 +53,36 @@ class Ticket:
 	def setProductCode(self, productCode):
 		self.__productCode = productCode
 
-	def setProduct(self, product):
-		self.__product = product
+	def setProductName(self, productName):
+		self.__productName = productName
+	
+	def setProductValue(self, productValue):
+		self.__productValue = productValue
 
 	def setMethodPayment(self, methodPayment):
 		self.__methodPayment = methodPayment
 	
 	# GETTERS
-	@property
 	def getID(self):
 		return self.__id
-
-	@property
-	def getEmployee(self):
-		return self.__employee
-
-	@property
+	
 	def getHour(self):
 		return self.__hour
 	
-	@property
 	def getDate(self):
 		return self.__date
 
-	@property
+	def getEmployee(self):
+		return self.__employee
+
 	def getProductCode(self):
 		return self.__productCode
 
-	@property
-	def getProduct(self):
-		return self.__product
+	def getProductName(self):
+		return self.__productName
 	
-	@property
+	def getProductValue(self):
+		return self.__productValue
+	
 	def getMethodPayment(self):
 		return self.__methodPayment
