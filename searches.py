7801df6bc192e565124515f3fba8734dbda65871
MoraRMod <ralerwip@gmail.com>
2023-05-19 23:34:46 -0600

    findCode, findNameSupplier ++

--- searches.py
+++ searches.py
@@ -1,4 +1,6 @@
 from client import Client
+from product import Product
+from supplier import Supplier
 
 def findID(gerente, id):
 	resultado = []
@@ -16,6 +18,26 @@ def findName(gerente, nombre):
 	for registro in gerente.getList():
 		for obj in registro:
 			if isinstance(obj, Client) and obj.getName() == nombre:
+				resultado.append(registro)
+
+	return resultado
+
+def findCode(gerente, id):
+	resultado = []
+
+	for registro in gerente.getList():
+		for obj in registro:
+			if isinstance(obj, Product) and obj.getCode() == id:
+				resultado.append(registro)
+
+	return resultado
+
+def findNameSupplier(gerente, id):
+	resultado = []
+
+	for registro in gerente.getList():
+		for obj in registro:
+			if isinstance(obj, Supplier) and obj.getName() == id:
 				resultado.append(registro)
 
 	return resultado
