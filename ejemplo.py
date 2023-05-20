b1dc9cee5d3f7ea9dd35e8bf55c18c83a73454f7
MoraRMod <ralerwip@gmail.com>
2023-05-19 23:22:49 -0600

    ejemplos de uso de algoritmos

--- ejemplo.py
+++ ejemplo.py
@@ -4,7 +4,7 @@ from modules import *
 gerente = Manager()
 index = 0
 
-while index <= 0:
+while index <= 1:
 	# Registros de longitud fija y variable
 	cliente = Client('102364', 'Omar', 'DOJA98DHHF9', '56 4651 7427', 'ralerwip@gmail.com')
 	fecha = Date('17', '05', '2023')
@@ -17,13 +17,66 @@ while index <= 0:
 	gerente.agregar([cliente, fecha, hora, producto, proovedor, empleado, recibo, prueba])
 
 	#gerente.mostrar()
-
+		
 	index += 1
 
-gerente.guardar('registros.json')
-gerente.abrir('registros.json')
+id = '102364'
+eliminarRegistro(gerente, id)
+
+print('\n\n')
+print('\n\n')
 
 gerente.mostrar()
+
+'''
+id_modificar = '102364'
+nuevo_nombre = 'Juan'
+nuevo_rfc = 'RGSK4645DS4'
+nuevo_tel = '26498842'
+nuevo_email = 'juan@example.com'
+
+modificarID(gerente, id_modificar, nuevo_nombre, nuevo_rfc, nuevo_tel, nuevo_email)
+
+print('\n\n')
+gerente.mostrar()
+'''
+
+'''
+id = '102364'
+resultados = findID(gerente, id)
+
+if resultados:
+	print(f"Resultados de la búsqueda para el ID '{id}':")
+
+	for registro in resultados:
+		for obj in registro:
+			print(obj)
+		print('\n')
+else:
+	print(f"No se encontraron resultados para el ID '{id}'.")
+
+print('\n\n')
+'''
+
+'''
+
+nombre = 'Omar'
+res = findName(gerente, nombre)
+
+if res:
+	print(f"Resultados de la búsqueda por nombre '{nombre}':")
+	for registro in res:
+		for obj in registro:
+			print(obj)
+else:
+	print(f"No se encontraron resultados para el nombre '{nombre}'.")
+'''
+
+#gerente.mostrar()
+
+#gerente.guardar('registros.json')
+#gerente.abrir('registros.json')
+#gerente.mostrar()
 
 '''
 # Indice simple
