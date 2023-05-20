21c0db5588fb9b350f751568e655b4646e0c0752
MoraRMod <ralerwip@gmail.com>
2023-05-19 23:50:48 -0600

    cambian los parametros

--- searches.py
+++ searches.py
@@ -22,22 +22,22 @@ def findName(gerente, nombre):
 
 	return resultado
 
-def findCode(gerente, id):
+def findCode(gerente, code):
 	resultado = []
 
 	for registro in gerente.getList():
 		for obj in registro:
-			if isinstance(obj, Product) and obj.getCode() == id:
+			if isinstance(obj, Product) and obj.getCode() == code:
 				resultado.append(registro)
 
 	return resultado
 
-def findNameSupplier(gerente, id):
+def findNameSupplier(gerente, name):
 	resultado = []
 
 	for registro in gerente.getList():
 		for obj in registro:
-			if isinstance(obj, Supplier) and obj.getName() == id:
+			if isinstance(obj, Supplier) and obj.getName() == name:
 				resultado.append(registro)
 
 	return resultado
